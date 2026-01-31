34a55d4be78ff96dcce631a7586507d0329ae42b
MIREGA LTDA. <app_mirega@mirega.cl>
2026-01-31 01:04:19 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import { AuthProvider, useAuth } from './contexts/AuthContext';
 import { LoginPage } from './components/LoginPage';
 import { SplashScreen } from './components/SplashScreen';
