8567f6f27dd5010a47c76dcc374c3881ba77997f
appmirega <appmirega@gmail.com>
2026-02-01 11:32:57 -0300

    Fix: forzar alias de react y react-dom en vite.config.ts para evitar doble carga

--- vite.config.ts
+++ vite.config.ts
@@ -14,6 +14,8 @@ export default defineConfig({
   },
   resolve: {
     alias: {
+      react: path.resolve(__dirname, './node_modules/react'),
+      'react-dom': path.resolve(__dirname, './node_modules/react-dom'),
       '@': path.resolve(__dirname, './src'),
     },
   },
