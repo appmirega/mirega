32d0b3a429a8e8aa63047d0188b2bfa0ced9f028
appmirega <appmirega@gmail.com>
2026-02-23 00:16:37 -0300

    Actualización de cambios para deploy

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,16 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
+import { AuthProvider } from './contexts/AuthContext';
 import './index.css';
 
 console.log('[main.tsx] Entrando a main.tsx');
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <AuthProvider>
+      <App />
+    </AuthProvider>
   </StrictMode>
 );
 
